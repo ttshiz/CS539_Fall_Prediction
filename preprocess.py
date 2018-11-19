ece7c1b7eadae9ed3a58e3f3fc2cc5c41376fa2c
ttshiz <ttshiz@wpi.edu>
2018-11-19 01:47:50 -0500

    modified preprocessing draft to be more csv ready

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sklearn as sk
 
+FALL_LABELS = set([b'FOL', b'FKL', b'BSC', b'SDL'])
+
 #one dimension
 def zero_crossings(slice):
     return np.where(np.diff(np.signbit(slice)))[0].size
@@ -11,20 +13,39 @@ def avg_slope(slice):
 
 # for each dimension still need to set
 def process_slice(slice):
-    return [np.amin(slice["acc_x"]), np.amin(slice["acc_y"]), np.amin(slice["acc_z"])
-            , np.amax(slice["acc_x"]), np.amax(slice["acc_y"]), np.amax(slice["acc_z"])
-            , np.std(slice["acc_x"]), np.std(slice["acc_y"]), np.std(slice["acc_z"])
-            , np.mean(slice["acc_x"]), np.mean(slice["acc_y"]), np.mean(slice["acc_z"])
-            , np.mean(np.diff(slice["acc_x"])), np.mean(np.diff(slice["acc_y"]))
-            , np.mean(np.diff(slice["acc_z"]))
-            , zero_crossings(slice["acc_x"]), zero_crossings(slice["acc_y"])
-            , zero_crossings(slice["acc_z"])
-            , avg_slope(slice["acc_x"]), avg_slope(slice["acc_y"])
-            , avg_slope(slice["acc_z"])
-            , slice["label"][0]]
+    slice_features = dict()
+    slice_features["x_min"] = np.amin(slice["acc_x"])
+    slice_features["y_min"] = np.amin(slice["acc_y"])
+    slice_features["z_min"] = np.amin(slice["acc_z"])
+    
+    slice_features["x_max"] = np.amax(slice["acc_x"])
+    slice_features["y_max"] = np.amax(slice["acc_y"])
+    slice_features["z_max"] = np.amax(slice["acc_z"])
+    
+    slice_features["x_std"] = np.std(slice["acc_x"])
+    slice_features["y_std"] = np.std(slice["acc_y"])
+    slice_features["z_std"] = np.std(slice["acc_z"])
+    
+    slice_features["x_mean"] = np.mean(slice["acc_x"])
+    slice_features["y_mean"] = np.mean(slice["acc_y"])
+    slice_features["z_mean"] = np.mean(slice["acc_z"])
+
+    slice_features["x_slope"] = np.mean(np.diff(slice["acc_x"]))
+    slice_features["y_slope"] = np.mean(np.diff(slice["acc_y"]))
+    slice_features["z_slope"] = np.mean(np.diff(slice["acc_z"]))
+
+    slice_features["x_zc"] = zero_crossings(slice["acc_x"])
+    slice_features["y_zc"] = zero_crossings(slice["acc_y"])
+    slice_features["z_zc"] = zero_crossings(slice["acc_z"])
+
+    # label each timeslice with the label of the last index unless it is a fall
+    falls = FALL_LABELS.intersection(set(slice["label"]))
+    slice_features["label"] = falls.pop() if falls else slice["label"][-1]
+
+    return slice_features
  
 def process_file(file_name, slice_size):
-    try:
+    #try:
         data = np.genfromtxt(file_name, dtype=None, delimiter=',', names=True)
         feature_list = []
         start_time = data[0]["timestamp"]
@@ -32,22 +53,17 @@ def process_file(file_name, slice_size):
 
         snum = 1
         slice_start = 0
-        slice_start_label = data[0]["label"]
         slice_end = 0
-        slice_end_label = slice_start_label
         while(slice_end < end_index):
             # if we still need to find the index of the end of the slice
-            if (data[slice_end]["timestamp"] < start_time + snum*slice_size) and (
-                    slice_start_label == slice_end_label):
+            if (data[slice_end]["timestamp"] < start_time + snum*slice_size):
                 slice_end = slice_end + 1
-                slice_end_label = data["label"][slice_end]
             # otherwise consider slice complete and process
             else:
                 feature_list.append(process_slice(data[slice_start:slice_end]))
                 snum = snum + 1
                 slice_start = slice_end
-                slice_start_label = slice_end_label
         return feature_list
-    except:
+    #except:
         print("Error Processing "+file_name)
         return False
