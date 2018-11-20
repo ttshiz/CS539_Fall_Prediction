98cfe418cc950d33fe71bee86fdc9ca580abb5be
ttshiz <ttshiz@wpi.edu>
2018-11-20 00:26:17 -0500

    revised preprocessing git add commit

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,4 @@
+import csv
 import glob
 import json
 import numpy as np
@@ -18,66 +19,77 @@ def min_max_distance(slice):
     
 # for each dimension still need to set
 def process_slice(slice):
+    #print(slice)
+    def slice_get(name):
+        return [float(v[name]) for v in slice]
     slice_features = dict()
-    slice_features["x_min"] = np.amin(slice["acc_x"])
-    slice_features["y_min"] = np.amin(slice["acc_y"])
-    slice_features["z_min"] = np.amin(slice["acc_z"])
+    slice_features["x_min"] = np.amin(slice_get("acc_x"))
+    slice_features["y_min"] = np.amin(slice_get("acc_y"))
+    slice_features["z_min"] = np.amin(slice_get("acc_z"))
     
-    slice_features["x_max"] = np.amax(slice["acc_x"])
-    slice_features["y_max"] = np.amax(slice["acc_y"])
-    slice_features["z_max"] = np.amax(slice["acc_z"])
+    slice_features["x_max"] = np.amax(slice_get("acc_x"))
+    slice_features["y_max"] = np.amax(slice_get("acc_y"))
+    slice_features["z_max"] = np.amax(slice_get("acc_z"))
     
-    slice_features["x_std"] = np.std(slice["acc_x"])
-    slice_features["y_std"] = np.std(slice["acc_y"])
-    slice_features["z_std"] = np.std(slice["acc_z"])
+    slice_features["x_std"] = np.std(slice_get("acc_x"))
+    slice_features["y_std"] = np.std(slice_get("acc_y"))
+    slice_features["z_std"] = np.std(slice_get("acc_z"))
     
-    slice_features["x_mean"] = np.mean(slice["acc_x"])
-    slice_features["y_mean"] = np.mean(slice["acc_y"])
-    slice_features["z_mean"] = np.mean(slice["acc_z"])
+    slice_features["x_mean"] = np.mean(slice_get("acc_x"))
+    slice_features["y_mean"] = np.mean(slice_get("acc_y"))
+    slice_features["z_mean"] = np.mean(slice_get("acc_z"))
 
-    slice_features["x_slope"] = np.mean(np.diff(slice["acc_x"]))
-    slice_features["y_slope"] = np.mean(np.diff(slice["acc_y"]))
-    slice_features["z_slope"] = np.mean(np.diff(slice["acc_z"]))
+    slice_features["x_slope"] = np.mean(np.diff(slice_get("acc_x")))
+    slice_features["y_slope"] = np.mean(np.diff(slice_get("acc_y")))
+    slice_features["z_slope"] = np.mean(np.diff(slice_get("acc_z")))
 
-    slice_features["x_zc"] = zero_crossings(slice["acc_x"])
-    slice_features["y_zc"] = zero_crossings(slice["acc_y"])
-    slice_features["z_zc"] = zero_crossings(slice["acc_z"])
+    slice_features["x_zc"] = zero_crossings(slice_get("acc_x"))
+    slice_features["y_zc"] = zero_crossings(slice_get("acc_y"))
+    slice_features["z_zc"] = zero_crossings(slice_get("acc_z"))
 
-    slice_features["x_mmd"] = min_max_distance(slice["acc_x"])
-    slice_features["y_mmd"] = min_max_distance(slice["acc_y"])
-    slice_features["z_mmd"] = min_max_distance(slice["acc_z"])
+    slice_features["x_mmd"] = min_max_distance(slice_get("acc_x"))
+    slice_features["y_mmd"] = min_max_distance(slice_get("acc_y"))
+    slice_features["z_mmd"] = min_max_distance(slice_get("acc_z"))
 
     # label each timeslice with the label of the majority class unless it is a fall
-    falls = FALL_LABELS.intersection(set(slice["label"]))
+    falls = FALL_LABELS.intersection(set([v["label"] for v in slice]))
     slice_features["label"] = falls.pop() if falls else Counter(
-        slice["label"]).most_common(1)[0][0]
+        [v["label"] for v in slice]).most_common(1)[0][0]
 
     return slice_features
  
 def process_file(file_name, slice_size):
     try:
-        data = np.genfromtxt(file_name, dtype=None, delimiter=',', names=True)
-        feature_list = []
-        start_time = data[0]["timestamp"]
-        end_index = data.size - 1
-
-        snum = 1
-        slice_start = 0
-        slice_end = 0
-        while(slice_end < end_index):
-            # if we still need to find the index of the end of the slice
-            if (data[slice_end]["timestamp"] < start_time + snum*slice_size):
-                slice_end = slice_end + 1
-            # otherwise consider slice complete and process
-            elif (slice_start < slice_end):
-                feature_list.append(process_slice(data[slice_start:slice_end]))
-                snum = snum + 1
-                slice_start = slice_end
+        #data = np.genfromtxt(file_name, dtype=None, delimiter=',', names=True)
+        with open(file_name, "r") as csv_file:
+            data = csv.DictReader(csv_file)
+            feature_list = []
+            snum = 1
+            cur_slice = list()
+            start_time = None
+            for d in data:
+                if not start_time:
+                    start_time = float(d["timestamp"])
+                # if we still need to find the index of the end of the slice
+                if (float(d["timestamp"]) < start_time + snum*slice_size):
+                    cur_slice.insert(0, d)
+                # otherwise consider slice complete and process
+                else:
+                    cur_slice.reverse()
+                    feature_list.insert(0, process_slice(cur_slice))
+                    cur_slice = list([d])
+                    snum = snum + 1
             else:
-                break
-        return feature_list
-    except:
-        print("Error Processing "+file_name)
+                if cur_slice:
+                    cur_slice.reverse()
+                    feature_list.insert(0, process_slice(cur_slice))
+                    cur_slice = list()
+                    snum = snum + 1
+            feature_list.reverse()
+            return feature_list
+    except Exception as e:
+        raise e
+        print("Error Processing "+file_name + " " + str(e))
         return False
 
 def process_directory(slice_size):
@@ -87,6 +99,6 @@ def process_directory(slice_size):
     return data
 
 def write_to_json(data):
-    with open("preprocessed"+str(data["slice_size"])+".json", "w") as fp:
+    with open("preprocessed_"+("%.1E"%data["slice_size"])+".json", "w") as fp:
         json.dump(data, fp)
 
