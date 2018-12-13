55aa394cd45566bc5be9869c992a44ad6fcd246d
ttshiz <ttshiz@wpi.edu>
2018-12-12 22:53:45 -0500

    Cleared up comments in log_reg.py

--- log_reg.py
+++ log_reg.py
@@ -22,19 +22,21 @@ def run_one_LR(X_train, Y_train, X_test, Y_test, prepro_param, rand_seed=None
     model.fit(X_train, Y_train)
     Y_predict = model.predict(X_test)
 
-    print("Running cross val "+str(run_count)+"....")    
+    print("Running cross val "+str(run_count)+"....")
+    # uncomment to save model
     #filename = "lr_pre_" + prepro_param + "solver_" + str(solver) + "iter_"
     #filename = filename + str(max_iter) + "run_" + str(run_count) + ".pkl"
     #joblib.dump(model, filename)
     #print(Y_predict)
     return Y_predict
 
-# does conversion from multi-class to binary
+# does conversion from multi-class labels to to binary
 def to_binary(Y_list):
     return [(n in preprocess.FALL_LABELS) for n in Y_list]
     
-# Runs k-fold Cross Validation on preprocessed data found in filename
-# with num_slices the number of preprocessed slices to include in each sample
+# Runs k-fold Cross Validation on preprocessed data found in the preprocessed file
+# pointed to by filename with num_slices the number of preprocessed slices to include
+# in each sample (uniform length in time)
 def do_cross_validation(filename, num_slices, k):
     print("Cross Validating: "+filename)
     data_gen = setup(filename, num_slices, k)
@@ -54,7 +56,8 @@ def do_cross_validation(filename, num_slices, k):
         prepro_param = filename[13:-5]
         Y_predicted = run_one_LR(X_train, Y_train, X_test, Y_train, prepro_param
                                  , run_count=run_num)
-        a, f, p, r, f_w, p_w, r_w, f_m, p_m, r_m = calc_metrics(Y_test, Y_predicted)
+        #a, f, p, r, f_w, p_w, r_w, f_m, p_m, r_m = calc_metrics(Y_test, Y_predicted) # binary version
+        a, f, p, r, f_w, p_w, r_w, f_m, p_m, r_m = calc_metrics(Y_test, Y_predicted) # multi-class version
         accuracies.append(a)
         f1s.append(f)
         precisions.append(p)
@@ -66,6 +69,7 @@ def do_cross_validation(filename, num_slices, k):
         precisions_micro.append(p_m)
         recalls_micro.append(r_m)
         run_num = run_num +1
+        # uncomment the print for verbose
         #print("accuracy: "+str(a)+" f1score: "+str(f)+" precision: "+str(p)+" recall: "
         #      +str(r)+" f1score weighted: "+str(f_w)+" precision weighted: "+str(p_w)
         #      +" recall weighted: "+str(r_w)+" f1score micro: "+str(f_m)
@@ -83,6 +87,7 @@ def do_cross_validation(filename, num_slices, k):
     scores["recalls_micro"] = recalls_micro
     return scores
 
+# Formats results of a cross validation run in human readable format and prints
 def print_by_cross_val_run(file_dict):
     stout = "\t mean \t max \n"+"acc \t"+str(file_dict["mean_acc"])+"\t"
     stout = stout + str(file_dict["max_acc"]) +"\n"
@@ -115,65 +120,54 @@ def print_by_cross_val_run(file_dict):
     stout = stout + str(file_dict["max_f1_micro"]) +"\n"
 
     print(stout)
-            
+
+# runs cross validation on the preprocessed data files    
 def main():
-    max_slice_size_file =  "preprocessed_6.0E+09.json"
     files = ["preprocessed_6.0E+09.json", "preprocessed_5.0E+09.json"
-             , "preprocessed_2.5E+09.json"
-             ,  "preprocessed_1.0E+09.json", "preprocessed_5.0E+08.json"
-             ,  "preprocessed_2.5E+08.json", "preprocessed_5.0E+07.json"
-             ,  "preprocessed_2.5E+07.json",  "preprocessed_5.0E+06.json"]
-    num_slices = dict()
+             , "preprocessed_2.5E+09.json",  "preprocessed_1.0E+09.json"]
+  
     # time_slice(nanoseconds) * number_of_slices <= 6 seconds
+    num_slices = dict()
     num_slices["preprocessed_6.0E+09.json"] = [1]
     num_slices["preprocessed_5.0E+09.json"] = [1]
     num_slices["preprocessed_2.5E+09.json"] = [1, 2]
     num_slices["preprocessed_1.0E+09.json"] = [1, 2, 4]
-    num_slices["preprocessed_5.0E+08.json"] = [1, 2, 4, 8]
-    num_slices["preprocessed_2.5E+08.json"] = [1, 2, 4, 8, 16]
-    num_slices["preprocessed_5.0E+07.json"] = [1, 2, 4, 8, 16, 32]
-    num_slices["preprocessed_2.5E+07.json"] = [1, 2, 4, 8, 16, 32, 64]
-    num_slices["preprocessed_5.0E+06.json"] = [1, 2, 4, 8, 16, 32, 64, 128]
-    
-    single_filename = "preprocessed_1.0E+09.json"
 
+    # k-fold cross-validation, for our purposes k = 10
     k = 10
 
-    #all_file_dict = dict()
+    # basic progress save system
     try:
         with open("bin_results_summary.json", "r") as infile:
             all_file_dict = json.loads(infile.read())
     except:
         all_file_dict = dict()
+        
+    # cross validate and calculate metrics for all preprocessed file -  num_slices pairs
     for filename in files:
         if filename in all_file_dict:
             continue
         file_dict = dict()
         try:
+            # do each pair
             for num_s in num_slices[filename]:
                 s = do_cross_validation(filename, num_s, k)
                 #print("scores "+str(s)) #enable for very verbose
-                with open("bin_metrics_slice_num_"+str(num_s)+"_"+filename, "w") as fp:
+                with open("metrics_slice_num_"+str(num_s)+"_"+filename, "w") as fp:
                     json.dump(s, fp)
                 fp.close()
                 print("aggregates:")
-                # want calc_cross_valaggregates for each run associated with run parameters
+                # calculate max and mean for metrics across the cross validation runs
                 file_dict = calc_cross_val_aggregates(s)
                 #print_by_cross_val_run(file_dict) #enable for verbose
-                #break; # comment out for full run
+                #break; # uncomment out to do one file
                 all_file_dict[filename+str(num_s)] = file_dict
         except ValueError:
             print("Error on file"+filename)
             continue
-            #break; # comment out for full run
+            #break; # uncomment out to do one file
         with open("bin_results_summary.json", "w") as fp:
             json.dump(all_file_dict, fp)
-          
-    # want confusion matrix for best pair of file and num_slices
-    # time provided runs:
-    #num_slices_reach = [256, 512, 1024]
-    #reach_files = ["preprocessed_5.0E+06.json"]
-    #print(s)
     return s
 
 if __name__ == "__main__":
